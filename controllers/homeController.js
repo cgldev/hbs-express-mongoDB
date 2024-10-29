28aafd7c9806426874a1eda94ae8156ffccedbef
Cesar Gimenez Lascano <cesargimenezlascano@gmail.com>
2024-10-29 16:56:13 -0300

    eliminar, validar, actualizar

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,7 @@
 const Url = require("../models/url");
 const { nanoid } = require("nanoid");
 
+//leer
 const leerUrls = async (req, res) => {
   try {
     const urls = await Url.find().lean();
@@ -11,6 +12,7 @@ const leerUrls = async (req, res) => {
   }
 };
 
+//agregar
 const agregarUrl = async (req, res) => {
   try {
     const { origin } = req.body;
@@ -19,11 +21,54 @@ const agregarUrl = async (req, res) => {
     res.redirect("/");
   } catch (error) {
     console.log(error);
-    res.send("algo fallo");
+    res.send("algo falló");
+  }
+};
+
+//eliminar
+const eliminarUrl = async (req, res) => {
+  try {
+    // tomo el parámetro
+    const { id } = req.params;
+    await Url.findByIdAndDelete(id);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    res.send("algo falló");
+  }
+};
+
+//editar Formulario
+const editarUrlForm = async (req, res) => {
+  // tomo el parámetro
+  const { id } = req.params;
+  try {
+    const urlDB = await Url.findById(id).lean();
+    //console.log(urlDB);
+    //vuelvo a renderizar home y envío el objeto
+    res.render("home", { urlDB });
+  } catch (error) {
+    console.log(error);
+    res.send("algo falló");
+  }
+};
+
+//editar
+const editarUrl = async (req, res) => {
+  const { id } = req.params;
+  const { origin } = req.body;
+  try {
+    await Url.findByIdAndUpdate(id, { origin: origin });
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
   }
 };
 
 module.exports = {
   leerUrls,
   agregarUrl,
+  eliminarUrl,
+  editarUrl,
+  editarUrlForm,
 };
